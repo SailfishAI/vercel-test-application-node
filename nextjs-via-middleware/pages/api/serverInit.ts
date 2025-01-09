06d6caaf7b0f569057fdd49b388548baed4e7679
hovhannisyangohar <gohar.hovhannisyan@sailfishqa.com>
2025-01-09 19:00:29 +0400

    Updated setupInterceptors function call.

--- nextjs-via-middleware/pages/api/serverInit.ts
+++ nextjs-via-middleware/pages/api/serverInit.ts
@@ -1,16 +1,14 @@
-import { addOrUpdateMetadata, identify, setupInterceptors } from "@sailfish/sf-veritas-nextjs";
-
-import dotenv from "dotenv";
-
-// Load environment variables if needed
-dotenv.config();
+import {
+  addOrUpdateMetadata,
+  identify,
+  setupInterceptors,
+} from "@sailfish/sf-veritas-nextjs";
 
 // Use a global variable to ensure setupInterceptors is only run once
 if (!(globalThis as any).__interceptors_initialized__) {
   console.log("Initializing interceptors in middleware...");
   setupInterceptors({
-    apiGraphqlEndpoint:
-    apiKey: process.env.API_KEY || "75ebed9a-b0dc-4a45-bd80-30b1516d8016",
+    apiKey: "75ebed9a-b0dc-4a45-bd80-30b1516d8016",
     domainsToNotPropagateHeadersTo: [
       "google.com",
       "https://app.sailfishqa.com",
@@ -18,7 +16,7 @@ if (!(globalThis as any).__interceptors_initialized__) {
       "https://github.com",
     ],
   });
-  identify('user11');
-  addOrUpdateMetadata('user11');
+  identify("user11");
+  addOrUpdateMetadata("user11");
   (globalThis as any).__interceptors_initialized__ = true;
 }
