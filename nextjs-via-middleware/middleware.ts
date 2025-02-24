b6e38559620a2274f9465de9ffd9cd9505af0478
hovhannisyangohar <gohar.hovhannisyan@sailfishqa.com>
2025-02-24 19:37:04 +0400

    Updated middleware.ts and package.json files.

--- nextjs-via-middleware/middleware.ts
+++ nextjs-via-middleware/middleware.ts
@@ -1,12 +1,12 @@
-import type { NextRequest } from 'next/server'
-import { NextResponse } from 'next/server'
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/home', request.url))
+  return NextResponse.next();
 }
 
-// See "Matching Paths" below to learn more
+// Apply middleware to all paths
 export const config = {
-  matcher: '/about/:path*',
+  matcher: '/:path*',
 }
